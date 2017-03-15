414da39aa003da91ad356f8b35c4df40176f93c3
Shreyansh Gupta <shrey.20may@gmail.com>
2017-03-15 14:10:27 +0530

    Update exercises.js

--- exercises.js
+++ exercises.js
@@ -15,7 +15,7 @@ function subtractFive(num) {
 }
 
 function areSameLength(str1, str2) {
-    if (str1.length == str2.length) {
+    if (str1.length === str2.length) {
         return true;
     }
     else {
